288db0daa756abcc49bc8522cff6349d42b5188a
Siddharth Vishwakarma <sid.sv14@gmail.com>
2022-05-10 15:53:21 +0530

    🐛 bug fixed

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,17 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { MdOutlineRestaurantMenu } from "react-icons/md";
-
 import images from "../../constants/images";
 import "./Navbar.css";
 
 const Navbar = () => {
-  const [toggleMenu, setToggleMenu] = useState(false);
-
+  const [toggleMenu, setToggleMenu] = React.useState(false);
   return (
     <nav className="app__navbar">
       <div className="app__navbar-logo">
-        <img src={images.gericht} alt="app logo" />
+        <img src={images.gericht} alt="app__logo" />
       </div>
       <ul className="app__navbar-links">
         <li className="p__opensans">
@@ -32,7 +30,7 @@ const Navbar = () => {
       </ul>
       <div className="app__navbar-login">
         <a href="#login" className="p__opensans">
-          Login/Register
+          Log In / Registration
         </a>
         <div />
         <a href="/" className="p__opensans">
@@ -46,27 +44,37 @@ const Navbar = () => {
           onClick={() => setToggleMenu(true)}
         />
         {toggleMenu && (
-          <div className="app__navbar-smallscreen_overlay flex__center side-bottom">
+          <div className="app__navbar-smallscreen_overlay flex__center slide-bottom">
             <MdOutlineRestaurantMenu
               fontSize={27}
               className="overlay__close"
               onClick={() => setToggleMenu(false)}
             />
             <ul className="app__navbar-smallscreen_links">
-              <li className="p__opensans">
-                <a href="#home">Home</a>
+              <li>
+                <a href="#home" onClick={() => setToggleMenu(false)}>
+                  Home
+                </a>
               </li>
-              <li className="p__opensans">
-                <a href="#about">About</a>
+              <li>
+                <a href="#about" onClick={() => setToggleMenu(false)}>
+                  About
+                </a>
               </li>
-              <li className="p__opensans">
-                <a href="#menu">Menu</a>
+              <li>
+                <a href="#menu" onClick={() => setToggleMenu(false)}>
+                  Menu
+                </a>
               </li>
-              <li className="p__opensans">
-                <a href="#awards">Awards</a>
+              <li>
+                <a href="#awards" onClick={() => setToggleMenu(false)}>
+                  Awards
+                </a>
               </li>
-              <li className="p__opensans">
-                <a href="#contact">Contact</a>
+              <li>
+                <a href="#contact" onClick={() => setToggleMenu(false)}>
+                  Contact
+                </a>
               </li>
             </ul>
           </div>
