9c1e31c8c51ebd3ea9a4b4145a72c853ec1a1d16
Siddharth Vishwakarma <sid.sv14@gmail.com>
2022-05-13 15:15:02 +0530

    bugs fix🐛

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -8,42 +8,39 @@ const AboutUs = () => (
     className="app__aboutus app__bg flex__center section__padding"
     id="about"
   >
-    {/* Background Image */}
-    <div className="app__aboutus-overlay flex-center">
-      <img src={images.G} alt="G" />
+    <div className="app__aboutus-overlay flex__center">
+      <img src={images.G} alt="G_overlay" />
     </div>
+
     <div className="app__aboutus-content flex__center">
-      {/* Left content */}
       <div className="app__aboutus-content_about">
         <h1 className="headtext__cormorant">About Us</h1>
-        <img src={images.spoon} alt="aboutus_spoon" className="spoon__img" />
+        <img src={images.spoon} alt="about_spoon" className="spoon__img" />
         <p className="p__opensans">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pharetra
           adipiscing ultrices vulputate posuere tristique. In sed odio nec
           aliquet eu proin mauris et.
         </p>
-        <button className="custom__button" type="button">
-          Know more
+        <button type="button" className="custom__button">
+          Know More
         </button>
       </div>
-      {/* Right content */}
-      <div className="app__aboutus-content_knife flex-center">
+
+      <div className="app__aboutus-content_knife flex__center">
         <img src={images.knife} alt="about_knife" />
       </div>
 
-      <div className="app__aboutus-content flex-center">
-        <div className="app__aboutus-content_about">
-          <h1 className="headtext__cormorant">About Us</h1>
-          <img src={images.spoon} alt="aboutus_spoon" className="spoon__img" />
-          <p className="p__opensans">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis
-            pharetra adipiscing ultrices vulputate posuere tristique. In sed
-            odio nec aliquet eu proin mauris et.
-          </p>
-          <button className="custom__button" type="button">
-            Know more
-          </button>
-        </div>
+      <div className="app__aboutus-content_history">
+        <h1 className="headtext__cormorant">Our History</h1>
+        <img src={images.spoon} alt="about_spoon" className="spoon__img" />
+        <p className="p__opensans">
+          Adipiscing tempus ullamcorper lobortis odio tellus arcu volutpat.
+          Risus placerat morbi volutpat habitasse interdum mi aliquam In sed
+          odio nec aliquet.
+        </p>
+        <button type="button" className="custom__button">
+          Know More
+        </button>
       </div>
     </div>
   </div>
